9488eb11b5f5531264db32c0753a6c41afc3d383
danbailey1000 <danbailey1000@msn.com>
2017-03-21 00:33:58 +0000

    First

---
+++ src/ContactsApplication/Data/IContactDataRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactsApplication.Model;
+
+namespace ContactsApplication.Data
+{
+    /// <summary>
+    /// Interface for data repository
+    /// </summary>
+    public interface IContactRepo
+    {
+        IEnumerable<ContactData> Get();
+        ContactData Get(int id);
+        void Add(ContactData contactData);
+        void Update(int id, ContactData contactData);
+        void Delete(int id);
+    }
+}
